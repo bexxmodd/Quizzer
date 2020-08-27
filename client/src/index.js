84bfb20fc7519040a25a2f28645721cd47854ea8
Subhasmita <2019003860.subhasmita@ug.sharda.ac.in>
2020-08-27 23:14:47 +0530

    upadted react component of login ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './login.css';
+import Login from "./Login";
+
+var destination = document.querySelector("#container");
 
 
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+
+  <div>
+    <Login/>
+  </div>,
+
+  destination
+
 );
