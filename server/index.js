4450c19c23a77d176855efc38d6e80636dded6b2
Souvik <souvikde.ns@gmail.com>
2020-08-31 04:11:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const next = require('next')
 const mongoose = require('mongoose')
 
-const dev = process.env.NODE_ENV === 'production'
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
