07c5b569bf76d3d76fa7fc51194635d10e2f0140
Souvik <souvikde.ns@gmail.com>
2020-09-02 17:48:32 +0530

    creating design

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,41 @@
+import Navbar from '../components/navbar'
 
-const Index = props => <>
-    <h1>Index Page</h1>
-</>
+import {
+    Container,
+    Grid,
+    Typography,
+    makeStyles
+} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        marginTop: theme.spacing(12)
+    },
+    title: {
+        marginTop: theme.spacing(20)
+    }
+}))
+
+const Index = props => {
+    const classes = useStyles()
+
+    return <div>
+        <Navbar />
+        <div className={classes.root}>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <img src="/images/questions.svg" alt="landing image" width="100%" />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="h4" align="center" className={classes.title} >
+                            Quizzer
+                    </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    </div>
+}
 
 export default Index
